5edbe36fc8f689d5b51839ddbefc979501020582
douhimed <mr.douhi.med@gmail.com>
2018-12-16 11:59:41 +0100

    tests fo MvcMock's NUllPointerExcepion in IntelliJ

--- src/main/java/org/mql/crowddonating/controllers/CaseController.java
+++ src/main/java/org/mql/crowddonating/controllers/CaseController.java
@@ -28,141 +28,147 @@ import java.util.*;
 @Controller
 public class CaseController {
 
-    @Autowired
-    @Qualifier("userBusiness")
-    private IUserServices userBusiness;
+	@Autowired
+	@Qualifier("userBusiness")
+	private IUserServices userBusiness;
 
-    @Autowired
-    @Qualifier("associationBusiness")
-    private IAssociationBusiness associationBusiness;
+	@Autowired
+	@Qualifier("associationBusiness")
+	private IAssociationBusiness associationBusiness;
 
-    @Autowired
-    @Qualifier("publicServicesBusiness")
-    private IPublicServices publicServices;
+	@Autowired
+	@Qualifier("publicServicesBusiness")
+	private IPublicServices publicServices;
 
-    @GetMapping("/cases")
-    public String cases(Model model, String... name) {
-        model.addAttribute("types", publicServices.getAllTypes());
-        if (name == null || name.length == 0)
-            model.addAttribute("cases", publicServices.getAllCases());
-        else {
-            model.addAttribute("name", name[0]);
-            model.addAttribute("cases", publicServices.getCasesByName("%" + name[0] + "%"));
-        }
-        return "cases/cases";
-    }
+	@GetMapping("/cases")
+	public String cases(Model model) {
+		model.addAttribute("types", publicServices.getAllTypes());
+		model.addAttribute("cases", publicServices.getAllCases());
+		return "cases/cases";
+	}
 
-    @GetMapping("/cases/{slug}")
-    public String caseBySlug(ModelMap map, @PathVariable String slug, HttpServletResponse response) {
-        Case aCase = publicServices.getCaseBySlug(slug);
-        if (aCase == null) {
-            response.setStatus(404);
-            return "error/404";
-        }
-        map.put("case", aCase);
-        return "cases/details";
-    }
+	@GetMapping("/cases/search/{name}")
+	public String cases(Model model, @PathVariable String name) {
+		model.addAttribute("types", publicServices.getAllTypes());
+		model.addAttribute("name", name);
+		model.addAttribute("cases", publicServices.getCasesByName("%" + name + "%"));
 
-    @DeleteMapping("/cases/{id}")
-    public String delete(@PathVariable long id, Model model) {
-        associationBusiness.deleteCase(id);
-        return "cases/cases";
-    }
+		return "cases/cases";
+	}
 
-    @GetMapping("/cases/add")
-    public String addForm(ModelMap map) {
-        Map<String, String> errors = new HashMap<>();
-        map.put("errors", errors);
-        map.put("aCase", new Case());
-        map.put("types", publicServices.getAllTypes());
-        return "cases/add";
-    }
+	@GetMapping("/cases/{slug}")
+	public String caseBySlug(ModelMap map, @PathVariable String slug, HttpServletResponse response) {
+		Case aCase = publicServices.getCaseBySlug(slug);
+		if (aCase == null) {
+			response.setStatus(404);
+			return "error/404";
+		}
+		map.put("case", aCase);
+		return "cases/details";
+	}
 
-    // @ResponseBody
-    @GetMapping("/cases/update/{slug}")
-    public String updateForm(ModelMap map, @PathVariable String slug, HttpServletResponse response) {
-        map.put("update", true);
-        Map<String, String> errors = new HashMap<>();
-        map.put("errors", errors);
-        Case aCase = publicServices.getCaseBySlug(slug);
-        if (aCase == null) {
-            response.setStatus(404);
-            return "error/404";
-        }
-        map.put("aCase", aCase);
-        return "cases/update";
-    }
+	@DeleteMapping("/cases/{id}")
+	public String delete(@PathVariable long id, Model model) {
+		associationBusiness.deleteCase(id);
+		return "cases/cases";
+	}
 
-    @PutMapping("/cases")
-    public String update(ModelMap map, Case aCase, @RequestParam MultipartFile imageFile, @RequestParam MultipartFile[] documents) {
-        Map<String, String> errors = new HashMap<>();
-        map.put("errors", errors);
-        map.put("aCase", aCase);
-        try {
-            associationBusiness.updateCase(aCase);
-        } catch (DataIntegrityViolationException ex) {
-            errors.put("name", "A case with the same name already exists!");
-            return "cases/add";
-        }
-        return "redirect:cases/" + aCase.getSlug();
-    }
+	@GetMapping("/cases/add")
+	public String addForm(ModelMap map) {
+		Map<String, String> errors = new HashMap<>();
+		map.put("errors", errors);
+		map.put("aCase", new Case());
+		map.put("types", publicServices.getAllTypes());
+		return "cases/add";
+	}
 
-    @PostMapping("/cases")
-    public String add(ModelMap map, Case aCase, String[] caseTypes, @RequestParam MultipartFile imageFile, @RequestParam MultipartFile[] documents) {
-        for (String ct : caseTypes) {
-            Type type = new Type();
-            type.setLabel(ct);
-            try {
-                associationBusiness.addType(type);
-            } catch (Exception ex) {
-                type = associationBusiness.findTypeByLabel(ct);
-            }
-            aCase.addType(type);
-        }
-        Map<String, String> errors = new HashMap<>();
-        map.put("errors", errors);
-        map.put("aCase", aCase);
-        try {
-            associationBusiness.addCase(aCase);
-            aCase.setImage(Utility.upload("images/cases/", imageFile));
-            associationBusiness.updateCase(aCase);
+	// @ResponseBody
+	@GetMapping("/cases/update/{slug}")
+	public String updateForm(ModelMap map, @PathVariable String slug, HttpServletResponse response) {
+		map.put("update", true);
+		Map<String, String> errors = new HashMap<>();
+		map.put("errors", errors);
+		Case aCase = publicServices.getCaseBySlug(slug);
+		if (aCase == null) {
+			response.setStatus(404);
+			return "error/404";
+		}
+		map.put("aCase", aCase);
+		return "cases/update";
+	}
 
-            for (MultipartFile doc : documents) {
-                File file = new File();
-                file.setPath(Utility.upload("files/cases/", doc));
-                file.setType("document");
-                file.setCase(aCase);
-                userBusiness.saveFile(file);
-            }
+	@PutMapping("/cases")
+	public String update(ModelMap map, Case aCase, @RequestParam MultipartFile imageFile,
+			@RequestParam MultipartFile[] documents) {
+		Map<String, String> errors = new HashMap<>();
+		map.put("errors", errors);
+		map.put("aCase", aCase);
+		try {
+			associationBusiness.updateCase(aCase);
+		} catch (DataIntegrityViolationException ex) {
+			errors.put("name", "A case with the same name already exists!");
+			return "cases/add";
+		}
+		return "redirect:cases/" + aCase.getSlug();
+	}
 
-        } catch (DataIntegrityViolationException ex) {
-            errors.put("name", "A case with the same name already exists!");
-            return "cases/add";
-        }
-        return "redirect:cases/" + aCase.getSlug();
-    }
+	@PostMapping("/cases")
+	public String add(ModelMap map, Case aCase, String[] caseTypes, @RequestParam MultipartFile imageFile,
+			@RequestParam MultipartFile[] documents) {
+		for (String ct : caseTypes) {
+			Type type = new Type();
+			type.setLabel(ct);
+			try {
+				associationBusiness.addType(type);
+			} catch (Exception ex) {
+				type = associationBusiness.findTypeByLabel(ct);
+			}
+			aCase.addType(type);
+		}
+		Map<String, String> errors = new HashMap<>();
+		map.put("errors", errors);
+		map.put("aCase", aCase);
+		try {
+			associationBusiness.addCase(aCase);
+			aCase.setImage(Utility.upload("images/cases/", imageFile));
+			associationBusiness.updateCase(aCase);
 
-    @InitBinder
-    private void DateBinder(WebDataBinder binder) {
-        SimpleDateFormat dateFormat = new SimpleDateFormat("MMM dd, yyyy");
-        CustomDateEditor editor = new CustomDateEditor(dateFormat, true);
-        binder.registerCustomEditor(Date.class, editor);
-    }
+			for (MultipartFile doc : documents) {
+				File file = new File();
+				file.setPath(Utility.upload("files/cases/", doc));
+				file.setType("document");
+				file.setCase(aCase);
+				userBusiness.saveFile(file);
+			}
 
-    @PostConstruct
-    public void createAssoc() {
-        Association assoc = new Association();
-        assoc.setId(1);
-        assoc.setEmail("assoc-123@gmail.com");
-        assoc.setUsername("assoc-1");
-        assoc.setName("assoc 1");
-        assoc.setCover("cover.jpg");
-        assoc.setBanned(false);
-        assoc.setAvatar("bd7dec8a-a7ab-4cfb-b1bc-2114a8662c55_akali.png");
+		} catch (DataIntegrityViolationException ex) {
+			errors.put("name", "A case with the same name already exists!");
+			return "cases/add";
+		}
+		return "redirect:cases/" + aCase.getSlug();
+	}
 
-        publicServices.addAssociation(assoc);
+	@InitBinder
+	private void DateBinder(WebDataBinder binder) {
+		SimpleDateFormat dateFormat = new SimpleDateFormat("MMM dd, yyyy");
+		CustomDateEditor editor = new CustomDateEditor(dateFormat, true);
+		binder.registerCustomEditor(Date.class, editor);
+	}
 
-        System.out.println("Assoc added.");
-    }
+	@PostConstruct
+	public void createAssoc() {
+		Association assoc = new Association();
+		assoc.setId(1);
+		assoc.setEmail("assoc-123@gmail.com");
+		assoc.setUsername("assoc-1");
+		assoc.setName("assoc 1");
+		assoc.setCover("cover.jpg");
+		assoc.setBanned(false);
+		assoc.setAvatar("bd7dec8a-a7ab-4cfb-b1bc-2114a8662c55_akali.png");
+
+		publicServices.addAssociation(assoc);
+
+		System.out.println("Assoc added.");
+	}
 
 }
--- src/test/java/org/mql/crowddonating/controllers/CaseControllerTest.java
+++ src/test/java/org/mql/crowddonating/controllers/CaseControllerTest.java
@@ -1,6 +1,5 @@
 package org.mql.crowddonating.controllers;
 
-import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -9,72 +8,54 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mql.crowddonating.business.IPublicServices;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.mql.crowddonating.business.implementations.PublicServicesBusiness;
 import org.mql.crowddonating.models.Case;
+import org.mql.crowddonating.models.Type;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+@RunWith(MockitoJUnitRunner.class)
 public class CaseControllerTest {
 
-	
 	private MockMvc mockMvc;
-	
-	/*
+
 	@InjectMocks
-	private CaseController caseController;
-	
+	private final CaseController caseController = new CaseController();
+
 	@Mock
-	private IPublicServices publicServices;
-	
-	Case case1;
-	Case case2;
-	
-	@Before
-	private void setup() {
-		caseController = new CaseController();
-		mockMvc = MockMvcBuilders.standaloneSetup(caseController).build();
-		
-		case1 = new Case();
-		case1.setId(1L);
-		case2 = new Case();
-		case2.setId(2L);
+	private PublicServicesBusiness publicServices;
+
+	@BeforeEach
+	void setup() {
 	}
 
-	
 	@Test
-	void shouldRenderCasesViewV2() throws Exception {
-		mockMvc.perform(get("/cases")).andExpect(view().name("cases/cases"));
-	}
-	
-	@Test
-	void cases_ShouldIncludeCasesInModel() throws Exception{ 
+	void cases_ShouldRenderCasesView() throws Exception {
+		
+		mockMvc = MockMvcBuilders.standaloneSetup(caseController).build();
+
+		Case case1 = new Case();
+		case1.setId(1);
+		Case case2 = new Case();
+		case2.setId(2);
 		List<Case> cases = Arrays.asList(case1, case2);
 		
+		List<Type> types = Arrays.asList(new Type());
+		
 		when(publicServices.getAllCases()).thenReturn(cases);
+
 		mockMvc.perform(get("/cases"))
 			.andExpect(status().isOk())
 			.andExpect(view().name("cases/cases"))
 			.andExpect(model().attribute("cases", cases));
-		
 		verify(publicServices).getAllCases();
-	}
-	*/
-	
-	private HomeController homeController;
-	
-	@Before
-	private void setup() {
-		homeController = new HomeController();
-		mockMvc = MockMvcBuilders.standaloneSetup(homeController).build();
-	}
 
-	
-	@Test
-	void shouldRenderCasesViewV2() throws Exception {
-		mockMvc.perform(get("/")).andExpect(view().name("home"));
 	}
 
 }
